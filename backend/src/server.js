ca7d379e62413f4dae98ca5574efff93209b1e1d
Corbin Jensen <corbinjensen@users.noreply.github.com>
2022-01-07 20:30:58 -0800

    added JSON Parsing support and tested with postman

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello',(req,res) => res.send('Hello!'));
+app.post('/hello',(req,res) => res.send(`Howdy ${req.body.name}`));
 
 app.listen(8000,() => console.log('Listening on port 8000'));
